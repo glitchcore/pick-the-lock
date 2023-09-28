35be802d5261dadb0015745f8dc1bad9e13a4bfa
coreglitch <mail@s3f.ru>
2023-09-28 21:40:21 +0400

    move and jump system

--- Actors/Player.gd
+++ Actors/Player.gd
@@ -1,12 +1,15 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-export var speed = Vector2(20000, 20000)
+export var speed = Vector2(2000, 2000)
 export var gravity = 5000
-export var jump_amount = 200000
+export var jump_amount = 5000
 
 func _ready() -> void:
 	pass
+
+var ui_mask = Vector2(1, 0)
+var gravity_mask = Vector2(0, 1)
 
 func _physics_process(delta: float) -> void:
 	var ui_direction = Vector2.ZERO
@@ -15,15 +18,34 @@ func _physics_process(delta: float) -> void:
 	ui_direction.y = (Input.get_action_strength("ui_down")
 		- Input.get_action_strength("ui_up"))
 		
-	var ui_mask = Vector2(1, 0)
-	var gravity_mask = Vector2(0, 1)
+	# check we are on the wall
+	if is_on_wall():
+		gravity_mask = gravity_mask.rotated(-PI/2)
+		
+	if is_on_ceiling():
+		gravity_mask = gravity_mask.rotated(PI)
+		
+	if abs(gravity_mask.x) > abs(gravity_mask.y):
+		ui_mask = Vector2(0, 1) 
+	else:
+		ui_mask = Vector2(1, 0)
 	
-	# jump is anti-gravity action
-	#  and is_on_floor()
-	var jump = 1 if Input.is_action_just_pressed("jump") else 0
-	jump *= -gravity_mask * jump_amount
+	# calculate ui movement
+	velocity = ((speed * ui_direction * ui_mask) +
+		velocity * (Vector2.ONE - ui_mask))
+		
+	# calculate gravity
+	velocity += gravity * gravity_mask * delta
 	
-	velocity = (speed * ui_direction * ui_mask +
-		gravity * gravity_mask + jump) * delta
+	# calculate jump is anti-gravity action
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity += -gravity_mask * jump_amount
+		
+	# interrupted jump
+	if Input.is_action_just_released("jump"):
+		if abs(gravity_mask.x) > 0:
+			velocity.x = 0
+		if abs(gravity_mask.y) > 0:
+			velocity.y = 0
 	
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, -gravity_mask)
